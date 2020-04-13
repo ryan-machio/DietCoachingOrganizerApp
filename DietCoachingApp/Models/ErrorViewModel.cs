ce8c3b8ed07964a8156f12b95bd5ad6d68eec166
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-12 23:10:54 -0400

    first commit

---
+++ DietCoachingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DietCoachingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
